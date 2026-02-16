db2e817c583548366c55d871d20ff47f7182eadb
nathan amare <NATHANAMARE@HOTMAIL.COM>
2026-02-16 19:15:13 +0300

    feat(ui): add Print PDF button to Generated Documents child table

--- tender_management/tender_management/doctype/tender_generated_document/tender_generated_document.js
+++ tender_management/tender_management/doctype/tender_generated_document/tender_generated_document.js
@@ -2,4 +2,12 @@ frappe.ui.form.on('Tender Generated Document', {
     // refresh: function(frm) {
 
     // }
+    print_pdf: function (frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        if (row.file) {
+            window.open(row.file, '_blank');
+        } else {
+            frappe.msgprint(__('No PDF file found for this document.'));
+        }
+    }
 });
